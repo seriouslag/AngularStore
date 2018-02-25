9f98a4e62c715c7887b78a79a174402f5789e124
seriouslag <Landon.Gavin@gmail.com>
2018-02-25 03:37:03 -0500

    fixed property name

--- src/app/components/dialogs/image/image.component.ts
+++ src/app/components/dialogs/image/image.component.ts
@@ -1,6 +1,7 @@
 import {Component, Inject, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 import {DomSanitizer} from '@angular/platform-browser';
+import {BehaviorSubject} from "rxjs/BehaviorSubject";
 
 @Component({
   selector: 'app-image',
@@ -10,8 +11,8 @@ import {DomSanitizer} from '@angular/platform-browser';
 export class ImageComponent implements OnInit, OnChanges {
 
   imageSrc: any;
-  loading = true;
-  failed = false;
+  isLoading$ = new BehaviorSubject<boolean>(false);
+  isFailed$ = new BehaviorSubject<boolean>(false);
 
   constructor(public dialogRef: MatDialogRef<ImageComponent>, @Inject(MAT_DIALOG_DATA) public data: any, private sanitizer: DomSanitizer) {
   }
@@ -35,22 +36,21 @@ export class ImageComponent implements OnInit, OnChanges {
 
   private getImageSrcByName(): void {
     if (this.data.image) {
-      this.loading = true;
-      this.data.apiService.getImageFileByImageId(this.data.image.imageID).take(1).subscribe(src => {
-        console.log('make it here', src);
+      this.isLoading$.next(true);
+      this.data.apiService.getImageFileByImageId(this.data.image.id).take(1).subscribe(src => {
         if (src != null) {
           const urlCreator = window.URL;
           this.imageSrc = this.sanitizer.bypassSecurityTrustUrl(urlCreator.createObjectURL(src));
-          this.failed = false;
+          this.isFailed$.next(false)
         }
-        this.loading = false;
+        this.isLoading$.next(false);
       }, () => {
-        this.loading = false;
-        this.failed = true;
+        this.isLoading$.next(false);
+        this.isFailed$.next(true);
       });
     } else {
-      this.loading = false;
-      this.failed = true;
+      this.isLoading$.next(false);
+      this.isFailed$.next(true);
     }
   }
 
