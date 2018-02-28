1fe59064c6ad6745a6522060f7d05390cb642b99
landon.gavin <landon.gavin@gmail.com>
2018-02-27 20:59:15 -0500

    update to auth layout and functionality

--- src/app/pages/admin/admin.page.component.ts
+++ src/app/pages/admin/admin.page.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {ApiService} from "../../services/api.service";
-import {AuthService} from "../../services/auth.service";
-import {Product} from "../../interfaces/product";
-import {ProductOption} from "../../interfaces/productOption";
-import {Observable} from "rxjs/Observable";
+import {ApiService} from '../../services/api.service';
+import {AuthService} from '../../services/auth.service';
+import {Product} from '../../interfaces/product';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
+import {ProductOption} from '../../interfaces/productOption';
 
 @Component({
   selector: 'app-admin-page',
@@ -12,6 +12,27 @@ import {Observable} from "rxjs/Observable";
 })
 export class AdminPageComponent implements OnInit {
 
+  postProductForm: FormGroup = new FormGroup({
+    name: new FormControl(null, [Validators.required, Validators.minLength(1)]),
+    productDescription: new FormControl(null, [Validators.required, Validators.minLength(1)])
+  });
+
+  postProductFormGroup = new FormGroup({
+    name: new FormControl(null, [Validators.required, Validators.minLength(1)]),
+    productDescription: new FormControl(null, [Validators.required, Validators.minLength(1)])
+  });
+
+  postProductOptionFormGroup: FormGroup = new FormGroup({
+    productOptions: new FormArray([])
+  });
+  /*{
+    name: new FormControl(null, [Validators.required, Validators.minLength(1)]),
+    price: new FormControl(null, [Validators.required, Validators.minLength(1)]),
+    productOptionDescription: new FormControl(null, [Validators.required, Validators.minLength(1)])
+  });*/
+
+  productOptions: ProductOption[];
+
   constructor(private apiService: ApiService, private authService: AuthService) { }
 
   ngOnInit() {
@@ -19,21 +40,23 @@ export class AdminPageComponent implements OnInit {
 
   async postProduct(): Promise<object> {
     const productTest = <Product>{
-      name: 'Beans',
-      tags: [],
-      productDescription: "The more you eat,",
-      productOptions: [<ProductOption>{
-        price: 10.20,
-        isVisible: true,
-        name: 'Black',
-        images: [],
-        productOptionDescription: "Tasty!"
-
-      }]
+      name: this.postProductForm.controls['name'].value,
+      productDescription: this.postProductForm.controls['productDescription'].value
     };
-    console.log('here');
 
     return this.apiService.postProduct(await this.authService.user$.getValue().getIdToken(), productTest).toPromise();
+  }
+
+  get productOptionsArray(): FormArray {
+    return (<FormArray>this.postProductOptionFormGroup.controls['productOptions']);
+  }
+
+  addProductOption(): void {
+    this.productOptionsArray.push(new FormGroup({
+      name: new FormControl(null, [Validators.required, Validators.minLength(1)]),
+      price: new FormControl(null, [Validators.required, Validators.minLength(1)]),
+      productOptionDescription: new FormControl(null, [Validators.required, Validators.minLength(1)])
+    }));
   }
 
 }
