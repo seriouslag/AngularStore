384d8400f6c155c10625ecf3e3b4f2f37e2a31a1
seriouslag <landon.gavin@gmail.com>
2018-03-07 19:46:00 -0500

    update toast duration. changed number 'type' to instance of number

--- src/app/services/toast.service.ts
+++ src/app/services/toast.service.ts
@@ -4,16 +4,15 @@ import {MatSnackBar} from '@angular/material';
 @Injectable()
 export class ToastService {
 
-  private duration: 2000;
+  private duration = 2000;
   private action = 'Ok';
 
   constructor(private snackBar: MatSnackBar) { }
 
-
   public open(message: string, action?: string, duration?: number): void {
-    this.snackBar.open(message, action ? action : this.action,
+    this.snackBar.open(message, (action ? action : this.action),
       {
-        duration: duration ? duration : this.duration
+        duration: (duration ? duration : this.duration)
       });
   }
 }
